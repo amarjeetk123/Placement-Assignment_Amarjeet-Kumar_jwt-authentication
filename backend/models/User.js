c26caa181cf2f1ebb2b3722f31f7d07b535a56ff
amarjeetk123 <ak123del@gmail.com>
2023-05-26 10:59:55 +0530

    User model is completed

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required : [true , "userName is required"]
+    },
+ 
+    email:{
+        type:String,
+        required : [true , "email is required"]
+    },
+    password:{
+        type:String,
+        required : [true , "password is required"]
+    },
+ 
+
+},
+{timestamps:true}
+
+
+)
+
+module.exports = mongoose.model("User" , userSchema )
